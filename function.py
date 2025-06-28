1e9adda94b68d299e0f5cc2440074b63e7d974ff
bhandigaregauri2006 <bhandigaregauri@gmail.com>
2025-06-28 22:08:39 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,42 @@
+# def msg():
+#     print("hello dolly are you")
+    
+# msg()
+    
+# def info(name):
+#     print("my name is :",name)
+# info("samiksha")
+
+# def info(name,lname,age,add):
+#     print(name,lname,age,add)
+# info("gauri","bhandigre",18,"pune")    
+# info("gauri","bhnadari",16,"mumbai") 
+
+# def data(*kid):
+#     print(kid)
+# data("radhika",12,"pune",5678904321)
+    
+#fun using for loop:
+
+# def food(fruits):
+#     for i in range(len(fruits)):
+#         print(i,fruits[i])
+# food(["mango","apple","kiwi","orange"])
+
+
+#add using fun
+
+# def addition(no1,no2):
+#     add=no1+no2
+#     print("add of two no is :",add)
+# addition(25,25)
+
+#sub using fun
+
+# def sub(no1,no2):
+#     return no1-no2
+# print(sub(25,5))
+
+
+data=lambda a,b:a+b
+print(data(85,98)) 
