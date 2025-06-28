e7b9aba9d2ee0fdd26bb8fadcb19f7782e967af3
bhandigaregauri2006 <bhandigaregauri@gmail.com>
2025-06-28 22:11:39 +0530

    Add files via upload

---
+++ ARRARY_FOR.py
@@ -0,0 +1,13 @@
+fruit = ["mango","apple","grapes","kiwi","banana","orange"]
+print(fruit)
+# for i in fruit:
+#     print(i)
+
+# for i in range (len (fruit)):
+#    print(i,fruit[i])
+#    print(i)
+
+# for i in fruit(5,0,-1):
+#     print(i)
+
+
