f68f2d7a4f5041a92af4fbd11963e23300ee3534
bhandigaregauri2006 <bhandigaregauri@gmail.com>
2025-06-28 22:12:03 +0530

    Add files via upload

---
+++ dai.py
@@ -0,0 +1,22 @@
+no=int(input("enter the no:"))
+for i in range(1,no+1,1):
+    for j in range(no+1,i,-1):
+        print(" ",end=" ")
+    for k in range(1,i+1,1):
+        print("* ",end="  ")
+    print()    
+for i in range(no-1,0,-1):
+    for j in range(no+1,i,-1):
+        print(" ",end=" ")
+    for k in range(1,i+1,1):
+        print("* ",end="  ")
+    print()        
+
+
+
+
+
+
+
+
+
