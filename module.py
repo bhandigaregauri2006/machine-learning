2d615e9443f1e71d85badad02cd9335b7501f860
bhandigaregauri2006 <bhandigaregauri@gmail.com>
2025-06-28 22:19:27 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,17 @@
+def msg():
+    print("welcome to kolhapur")
+
+def info(name,age,add,prof):
+    print(name,age,add,prof)    
+
+def data(mob,rollno,add):
+    print(mob,rollno,add)
+
+college="kolhapur new clg"
+car=["BMW","LAMBORGANO","MUSHTANG","FARARI","THAR","GT-650"]
+student={
+    "name":"Gauri",
+    "rollno":19
+
+}
+    
