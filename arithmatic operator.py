60bf361fbb38f43bcf9dae1eb42df97408f5953e
bhandigaregauri2006 <bhandigaregauri@gmail.com>
2025-06-28 22:10:38 +0530

    Add files via upload

---
+++ arithmatic operator.py
@@ -0,0 +1,15 @@
+no1=int(input("enter the no1 :"))
+no2 = int(input("entrer the no2 :"))
+#addition
+add = no1+no2
+print("addition is:",add)
+#subtraction 
+sub = no1-no2
+print("sub is"+str(sub))
+#multiplication
+mul = no1*no2
+print("multification is:"+str(mul))
+#division
+div = no1/no2
+print("division is:",div)
+
