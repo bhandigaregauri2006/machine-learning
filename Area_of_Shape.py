f485f625e0807d805033f4b9f949c836cee2119d
bhandigaregauri2006 <bhandigaregauri@gmail.com>
2025-06-28 22:11:04 +0530

    Add files via upload

---
+++ Area_of_Shape.py
@@ -0,0 +1,16 @@
+#rectangle Area
+len = int(input("enter the length :"))
+wit = int(input("enter the Width:"))
+area = len*wit
+print("area is :",area)
+
+#Area of Square
+areaS = len*len
+print("Area of square:",areaS)
+
+#Area of circle
+pi = 3.14
+rad = int(input("enter the radian:"))
+areaC =pi*rad*rad
+print("Area of circle:",areaC)
+
