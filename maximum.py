fe18676ef193ce009124df9220c940d85e2d6697
bhandigaregauri2006 <bhandigaregauri@gmail.com>
2025-06-28 22:17:51 +0530

    Add files via upload

---
+++ maximum.py
@@ -0,0 +1,8 @@
+no1 = int(input("enter the number"))
+no2 = int(input("enter the number"))
+if no1>no2:
+    print("no1 is maximum")
+elif no2>no1:
+    print("no2 is maximum")
+else:
+    print("all same")
