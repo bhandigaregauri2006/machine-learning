51443586017f6480d57ffe0dc635b452ebc3f1cc
bhandigaregauri2006 <bhandigaregauri@gmail.com>
2025-06-28 22:17:19 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,54 @@
+fruit=["mango","banana","apple","kiwi","grapes","orange","cheri"]
+# print(fruit)
+# print(fruit[0])
+# print(fruit[2:5])
+# print(fruit[:5])
+# print(fruit[0:])
+# print(fruit[:])
+
+# #change
+# fruit[3]="watermelon"
+# print(fruit)
+
+# #append
+# print(fruit.append("pineapple"))
+# print(fruit)
+
+# print(fruit.insert(2,"strawbery"))
+#print(fruit)   #insert
+
+# print(fruit.remove("grapes"))
+# print(fruit)   #remove
+
+# print(fruit.pop())
+# print(fruit)   #pop
+
+# print(fruit.clear())
+# print(fruit)   #cleare
+
+# for i in range(len(fruit)):
+#     print(str(i)+" "+fruit[i])
+
+# i=0
+# while i<len(fruit):
+#     print(str(i)+" "+str(fruit[i]))
+#     i+=1
+
+# print(fruit.sort())
+# print(fruit)
+
+# print(fruit.sort(reverse= True))
+# print(fruit)
+
+# print(fruit.copy())
+
+# friends= ["all batch"]
+# print(friends)
+# print(fruit+friends)
+
+# print(fruit.count("mango")) #count
+
+#print(fruit.index("banana"))
+
+print(fruit.reverse())
+print(fruit)
