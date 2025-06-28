53655f3fe845da5b20b8c36ed54fa0e4ac52d7ee
bhandigaregauri2006 <bhandigaregauri@gmail.com>
2025-06-28 22:15:12 +0530

    Add files via upload

---
+++ for_break.py
@@ -0,0 +1,14 @@
+# actor = ["A","D","N","G","E"]
+# print(actor)
+# for i in actor :
+#       #print(i)
+#       if i =="D":
+#         print(i)
+#         break
+
+
+#Continue
+for i in range(1,6,1):
+    if i == 3:
+        continue
+    print(i)     ``
